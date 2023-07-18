d11f062ac6c774a86f7ce25e2c89dc2ca9e67a49
Shruti Chindarkar <shrutic629@gmail.com>
2023-07-19 01:08:41 +0530

    Added alphabet detector program

---
+++ 18_july_2023/alphabet_detector.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() 
+{
+    char a;
+    printf("enter the character");
+    scanf("%c", &a);
+    if((a>='a' && a<='z') || (a>='A' && a<='Z'))
+    {
+        printf("%c is an alphabet", a);
+    }
+    else
+    {
+        printf("%c is not an alphabet", a);
+    }
+    return 0;
+}
